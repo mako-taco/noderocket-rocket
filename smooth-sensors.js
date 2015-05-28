af9e9ed5a57238362f00881a28e686c548fc7ff6
Jake Scott <jscott@addthis.com>
2015-05-28 17:20:03 -0400

    sadsad

--- smooth-sensors.js
+++ smooth-sensors.js
@@ -21,24 +21,24 @@ SmoothSensors.prototype = {
 	 * @return {RocketData[]}
 	 */
 	removeOutliers: function (field) {
-		this.quartileIndex1 = Math.floor(this.data.length * .25);;
-		this.quartileIndex3 = Math.floor(this.data.length * .75);;
-		this.medianIndex = Math.floor(this.data.length * .50);;
-		let data = this.data.slice().sort((a, b) => a - b);
-		let interQuartileDistance = data[this.quartileIndex3] - data[quartileIndex1];
-		let median = data[this.medianIndex];
-		let min = median - 1.5 * interQuartileDistance;
-		let max = median + 1.5 * interQuartileDistance;
+		this.quartileIndex1 = Math.floor(this.data.length * .25);
+		this.quartileIndex3 = Math.floor(this.data.length * .75);
+		this.medianIndex = Math.floor(this.data.length * .50);
+		var data = this.data.slice().sort((a, b) => a - b);
+		var interQuartileDistance = data[this.quartileIndex3] - data[this.quartileIndex1];
+		var median = data[this.medianIndex];
+		var min = median - 1.5 * interQuartileDistance;
+		var max = median + 1.5 * interQuartileDistance;
 
 		return this.data.filter(datum => {
-			let val = datum[field];
+			var val = datum[field];
 			return val < max && val > min;
 		});
 	},
 
 	getAltitude: function() {
-		let noOutliers = this.data.removeOutliers('alt');
-		let sum = noOutliers.map(x => x.alt).reduce((a, b) => a + b);
+		var noOutliers = this.data.removeOutliers('alt');
+		var sum = noOutliers.map(x => x.alt).reduce((a, b) => a + b);
 
 		return sum / noOutliers.length;
 	}
