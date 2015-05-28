262f6a9b85d7143013108892957ef4a24aac8e0e
Jake Scott <jscott@addthis.com>
2015-05-28 15:22:31 -0400

    ..

---
+++ altitude-timer.js
@@ -0,0 +1,37 @@
+class AltitudeTimer {
+	constructor(threshold = 0) {
+		this.previousAltitude = 0;
+		this.previousTime = 0;
+		this.altitude = 0;
+		this.time = 0;
+
+		this.velocity = 0;
+		this.threshold = threshold;
+
+		this.firstTime = true;
+		this.deploy = false;
+	}
+
+	mark(data) {
+		this.previousTime = this.time;
+		this.time = data.dt.getTime();
+		this.previousAltitude = this.altitude;
+		this.altitude = data.alt;
+
+		let dt = (this.time - this.previousTime);
+		let da = (this.altitude - this.previousAltitude);
+
+		this.velocity = da / dt;
+
+		if (this.firstTime) {
+			this.firstTime = false;
+		}
+		else if (Math.abs(this.velocity) < this.threshold) {
+			this.deploy = true;
+		}
+	}
+
+	shouldDeploy() {
+		return this.deploy;
+	}
+}
--- modules/parachute-smart-module.js
+++ modules/parachute-smart-module.js
@@ -1,30 +1,59 @@
+'use strict';
+
 import RocketModule from '../rocket-module';
 import SmoothSensors from '../smooth-sensors';
+import AltitudeTimer from '../altitude-timer';
+
+const PRE_LAUNCH = 'PRE_LAUNCH';
+const LAUNCHED = 'LAUNCHED';
+const AFTER_CHUTE = 'AFTER_CHUTE';
+
 
 class ParachuteModule extends RocketModule {
 	constructor(rocket, io) {
 		super('parachute-smart', rocket, io);
 		this.smoothSensors = new SmoothSensors();
+		this.altitudeTimer = new AltitudeTimer(.5);
+		this.state = PRE_LAUNCH;
 	}
 
 	onRocketReady() {
-		console.log('Rocket ready!');
+		module.log('Rocket ready!');
 	}
 
 	onRocketData(datum) {
+		this.log('[%s] %s', this.getName(), this.state);
+
 		this.smoothSensors.mark(datum);
-		console.log(this.smoothSensors.getAltitude());
+
+		switch (this.state) {
+			case PRE_LAUNCH:
+				if (datum.ay < -2) {
+					this.state = LAUNCHED;
+				}
+				break;
+			case LAUNCHED:
+				this.altitudeTimer.mark(datum);
+				if (this.altitudeTimer.shouldDeploy()) {
+					this.rocket.deployParachute();
+					this.state = AFTER_CHUTE;
+				}
+				break;
+			case AFTER_CHUTE:
+				break;
+
+		}
 	}
 
 	doEnable() {
-		if(!this.enabled) {
+		if (!this.enabled) {
 			this.rocket.on('rocket.ready', this.onRocketReady);
 			this.rocket.on('rocket.data', this.onRocketData);
 		}
 	}
 
 	doDisable() {
-		if(this.enabled) {
+		if (this.enabled) {
 			this.rocket.removeListener('rocket.ready', this.onRocketReady);
 			this.rocket.removeListener('rocket.data', this.onRocketData);
 		}
